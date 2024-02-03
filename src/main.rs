68735b5bc85cbacfeb24e51005bf7613cee560e7
Colin Wilson <github@colinwilson.dev>
2024-02-03 12:21:50 -0500

    feat: :sparkles: Do it functionally and add tests

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,42 @@
 use std::io::{self};
 
 fn main() {
-    let mut input = String::new();
-
-    let mut total: i32 = 0;
-
     let lines = io::stdin().lines();
 
-    for line in lines {
-        let line = line.unwrap();
-        if line.len() == 0 {
-            break;
-        }
-        let num: i32 = line.trim().parse().unwrap_or(0);
-        total += num;
-    }
+    let total = get_sum_lines(lines.map(|line| line.unwrap()));
 
     println!("total: {}", total);
+}
+
+fn get_sum_lines<I: Iterator<Item = String>>(lines: I) -> i32 {
+    lines
+        .take_while(|line| line.len() > 0)
+        .filter_map(|line| line.trim().parse::<i32>().ok())
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_sum_all_ints() {
+        let test_data = ["1", "2", "3"].iter().map(|item| item.to_string());
+
+        assert_eq!(get_sum_lines(test_data), 6);
+    }
+
+    #[test]
+    fn should_ignore_after_empty() {
+        let test_data = ["1", "2", "3", "", "4"].iter().map(|item| item.to_string());
+
+        assert_eq!(get_sum_lines(test_data), 6);
+    }
+
+    #[test]
+    fn should_ignore_non_ints() {
+        let test_data = ["1", "2", "dhjds", "3"].iter().map(|item| item.to_string());
+
+        assert_eq!(get_sum_lines(test_data), 6);
+    }
 }
